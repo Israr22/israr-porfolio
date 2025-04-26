f94365614c787fe7757cb8bf2b35c4237db0a802
Mohd Israr <israrraza942001@gmail.com>
2025-04-27 00:51:21 +0530

    add analyticts for varcel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.scss";
 import Main from "./containers/Main";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
     <div>
+      <SpeedInsights />
       <Main />
     </div>
   );
